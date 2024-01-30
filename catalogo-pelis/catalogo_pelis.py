2b611ed05a9e2c8e1da073c2f68f4109a6f31160
angelgh04 <angelrivero.tec@gmail.com>
2024-01-30 09:54:40 -0300

    Se acutalizó los comentarios de línea y se agregó los botones nuevo, guardar, cancelar, grilla de registros y los botones editar y cancelar. Continuar con la BBDD

--- catalogo-pelis/catalogo_pelis.py
+++ catalogo-pelis/catalogo_pelis.py
@@ -1,21 +1,27 @@
-#Es nuestro módulo principal del proyecto
+#!/usr/bin/env python3
+#coding=utf-8
 
+#Es nuestro módulo principal que contendra el entrypoint y la función principal
+#Importamos la librería tkinder con el alias tk
 import tkinter as tk
 #Importamos las clases del módulo gui_app
 from client.gui_app import Frame, barra_menu
 
+#Definimos la función principal main()
 def main():
     root=tk.Tk()
-    root.title('Catálogo de Películas')
+    root.title('Catálogo de Películas en Python')
     #root.iconbitmap('img/cp-logo.ico')
+    #Para que las dimensiones de la ventana no sea modificable
     root.resizable(0,0)
     
     barra_menu(root)
     
-    #Definimos un objeto de la clase Frame
+    #Definimos un objeto de la clase Frame (contenedor para formularios)
     app = Frame(root =root)
-    
+    #Indica el final de la ejecución de nuestra aplicación
     app.mainloop()
-    
+
+#Definimos el entrypoint
 if __name__ == '__main__':
     main()
